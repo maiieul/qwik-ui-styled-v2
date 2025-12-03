4f0e3c7f23ae22af7040744afb4d1395f6687acc
maiieul <45822175+maiieul@users.noreply.github.com>
2025-12-03 16:35:52 +0100

    chore: test codecov

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -10,26 +10,38 @@ import styles from "./input.css?inline";
 type InputProps = PropsOf<"input"> & {
   onInput$?: QRL<QRLEventHandlerMulti<InputEvent, HTMLInputElement>>;
   "data-testid"?: string;
+  error?: string;
 };
 
 export const Input = component$<InputProps>(
-  ({ name, id, value, onInput$, "data-testid": dataTestId, ...props }) => {
+  ({
+    name,
+    id,
+    value,
+    onInput$,
+    "data-testid": dataTestId,
+    error,
+    ...props
+  }) => {
     useStyles$(styles);
     const inputId = id || name;
 
     return (
-      <input
-        {...props}
-        // must be declared for two-way binding to work
-        bind:value={props["bind:value"]}
-        // must be declared for one-way binding to work
-        value={value}
-        onInput$={onInput$}
-        class={["input", props.class]}
-        id={inputId}
-        name={name}
-        data-testid={dataTestId && `input_${dataTestId}`}
-      />
+      <>
+        <input
+          {...props}
+          // must be declared for two-way binding to work
+          bind:value={props["bind:value"]}
+          // must be declared for one-way binding to work
+          value={value}
+          onInput$={onInput$}
+          class={["input", props.class]}
+          id={inputId}
+          name={name}
+          data-testid={dataTestId && `input_${dataTestId}`}
+        />
+        {error && <div>{error}</div>}
+      </>
     );
   },
 );
