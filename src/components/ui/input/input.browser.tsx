b3ae8a0b1bafc45b25a6c413e8c7219d63878f8c
maiieul <45822175+maiieul@users.noreply.github.com>
2025-12-03 08:57:38 +0100

    test: more input tests

--- src/components/ui/input/input.browser.tsx
+++ src/components/ui/input/input.browser.tsx
@@ -1,11 +1,10 @@
-import { component$ } from "@qwik.dev/core";
+import { component$, useSignal } from "@qwik.dev/core";
 import { expect, describe, it } from "vitest";
 import { page } from "vitest/browser";
 import { render } from "vitest-browser-qwik";
 import { Input } from "./input";
 
 const inputEl = (dataTestId: string) => page.getByTestId(`input_${dataTestId}`);
-// const errorEl = page.getByTestId(`input_error_${inputId}`);
 
 const Basic = component$(() => {
   return (
@@ -15,129 +14,71 @@ const Basic = component$(() => {
   );
 });
 
-describe("Input", () => {
-  it("should be visible", async () => {
-    render(<Basic />);
-    await expect.element(inputEl("basic")).toBeVisible();
-  });
+const WithPlaceholder = component$(() => {
+  return (
+    <div>
+      <Input data-testid="placeholder" placeholder="test placeholder" />
+    </div>
+  );
 });
 
-// import { component$, JSXNode, useSignal } from '@builder.io/qwik';
-// import { QwikCityMockProvider } from '@builder.io/qwik-city';
+const WithDefaultValue = component$(() => {
+  return (
+    <div>
+      <Input data-testid="value" value="test value" />
+    </div>
+  );
+});
 
-// import { inputSelectors } from '../../../cypress/support/po/input.selectors';
-// import { Input } from './input';
+const WithBindValue = component$(() => {
+  const valueSig = useSignal("test value");
+  return (
+    <div>
+      <Input data-testid="bind" bind:value={valueSig} />
+    </div>
+  );
+});
 
-// const InputInstance = component$<typeof Input>((props) => {
-//   return <Input {...props} id="test" />;
-// });
+describe("Input", () => {
+  describe("standalone", () => {
+    it("should be visible", async () => {
+      render(<Basic />);
+      await expect.element(inputEl("basic")).toBeVisible();
+    });
 
-// const InputTemplate = ({ ...props }) => {
-//   return (
-//     <QwikCityMockProvider>
-//       <InputInstance {...props} />
-//     </QwikCityMockProvider>
-//   );
-// };
+    it("should be empty by default", async () => {
+      render(<Basic />);
+      await expect.element(inputEl("basic")).toHaveValue("");
+    });
 
-// describe('Input', () => {
-//   describe('standalone', () => {
-//     beforeEach(() => {
-//       cy.viewport('iphone-6');
+    it("should be filled when typed", async () => {
+      render(<Basic />);
+      const el = inputEl("basic");
+      await el.fill("test");
+      await expect.element(el).toHaveValue("test");
+    });
+  });
 
-//       cy.mount((<InputTemplate />) as JSXNode);
-//     });
+  describe("with placeholder", () => {
+    it("should render with a placeholder", async () => {
+      render(<WithPlaceholder />);
+      await expect
+        .element(inputEl("placeholder"))
+        .toHaveAttribute("placeholder", "test placeholder");
+    });
+  });
 
-//     it('should render', () => {
-//       inputSelectors.getInputInputById('test').should('exist');
-//       inputSelectors.getInputInputById('test').should('be.visible');
-//     });
-//     it('should be empty by default', () => {
-//       inputSelectors.getInputInputById('test').should('have.value', '');
-//     });
-//     it('should be filled when typed', () => {
-//       inputSelectors.getInputInputById('test').type('test');
-//       inputSelectors.getInputInputById('test').should('have.value', 'test');
-//     });
-//     it('should not be required by default', () => {
-//       inputSelectors.getInputInputById('test').should('not.have.attr', 'required');
-//       inputSelectors.getInputRequiredStarById('test').should('not.exist');
-//     });
-//   });
+  describe("with a default value", () => {
+    it("should render with a default value", async () => {
+      render(<WithDefaultValue />);
+      await expect.element(inputEl("value")).toHaveValue("test value");
+    });
+  });
 
-//   describe('with label', () => {
-//     beforeEach(() => {
-//       cy.mount((<InputTemplate label="test label" />) as JSXNode);
-//     });
-//     it('should render with label', () => {
-//       inputSelectors.getInputInputById('test').should('exist');
-//       inputSelectors.getInputLabelById('test').should('have.text', 'test label');
-//       inputSelectors.getInputRequiredStarById('test').should('not.exist');
-//     });
-//   });
-
-//   describe('with error', () => {
-//     beforeEach(() => {
-//       cy.mount((<InputTemplate error="test error" />) as JSXNode);
-//     });
-//     it('should render with an error message', () => {
-//       inputSelectors.getInputInputById('test').should('exist');
-//       inputSelectors.getInputErrorById('test').should('have.text', 'test error');
-//     });
-//   });
-
-//   describe('with placeholder', () => {
-//     beforeEach(() => {
-//       cy.mount((<InputTemplate placeholder="test placeholder" />) as JSXNode);
-//     });
-//     it('should render with a placeholder', () => {
-//       inputSelectors.getInputInputById('test').should('exist');
-//       inputSelectors
-//         .getInputInputById('test')
-//         .should('have.attr', 'placeholder', 'test placeholder');
-//     });
-//   });
-
-//   describe('for a required field', () => {
-//     beforeEach(() => {
-//       cy.mount((<InputTemplate label="test label" required />) as JSXNode);
-//     });
-//     it('should render with a required star', () => {
-//       inputSelectors.getInputInputById('test').should('have.attr', 'required', 'required');
-//       inputSelectors.getInputRequiredStarById('test').should('be.visible');
-//     });
-//   });
-
-//   describe('with a default value', () => {
-//     beforeEach(() => {
-//       cy.mount((<InputTemplate value="test value" />) as JSXNode);
-//     });
-//     it('should render with a default value', () => {
-//       inputSelectors.getInputInputById('test').should('exist');
-//       inputSelectors.getInputInputById('test').should('have.value', 'test value');
-//     });
-//   });
-
-//   describe('with a bind:value', () => {
-//     const BoundInputInstance = component$<typeof Input>((props) => {
-//       const valueSig = useSignal('test value');
-//       return <Input {...props} id="test" bind:value={valueSig} />;
-//     });
-
-//     const BoundInputTemplate = ({ ...props }) => {
-//       return (
-//         <QwikCityMockProvider>
-//           <BoundInputInstance {...props} />
-//         </QwikCityMockProvider>
-//       );
-//     };
-
-//     beforeEach(() => {
-//       cy.mount((<BoundInputTemplate />) as JSXNode);
-//     });
-//     it('should render with a default value', () => {
-//       inputSelectors.getInputInputById('test').should('exist');
-//       inputSelectors.getInputInputById('test').should('have.value', 'test value');
-//     });
-//   });
-// });
+  describe("with a bind:value", () => {
+    it("should render with a default value", async () => {
+      render(<WithBindValue />);
+      await expect.element(inputEl("bind")).toHaveValue("test value");
+    });
+  });
+});
--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -8,48 +8,28 @@ import {
 import styles from "./input.css?inline";
 
 type InputProps = PropsOf<"input"> & {
-  error?: string;
   onInput$?: QRL<QRLEventHandlerMulti<InputEvent, HTMLInputElement>>;
   "data-testid"?: string;
 };
 
 export const Input = component$<InputProps>(
-  ({
-    name,
-    error,
-    id,
-    value,
-    onInput$,
-    "data-testid": dataTestId,
-    ...props
-  }) => {
+  ({ name, id, value, onInput$, "data-testid": dataTestId, ...props }) => {
     useStyles$(styles);
     const inputId = id || name;
 
     return (
-      <>
-        <input
-          {...props}
-          aria-errormessage={`${inputId}-error`}
-          aria-invalid={!!error}
-          bind:value={props["bind:value"]}
-          value={value}
-          onInput$={onInput$}
-          class={["input", props.class]}
-          id={inputId}
-          name={name}
-          data-testid={dataTestId && `input_${dataTestId}`}
-        />
-        {error && (
-          <div
-            id={`${inputId}-error`}
-            class="input-error"
-            data-testid={dataTestId && `input_error_${dataTestId}`}
-          >
-            {error}
-          </div>
-        )}
-      </>
+      <input
+        {...props}
+        // must be declared for two-way binding to work
+        bind:value={props["bind:value"]}
+        // must be declared for one-way binding to work
+        value={value}
+        onInput$={onInput$}
+        class={["input", props.class]}
+        id={inputId}
+        name={name}
+        data-testid={dataTestId && `input_${dataTestId}`}
+      />
     );
   },
 );
